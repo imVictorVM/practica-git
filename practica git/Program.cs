3f6dc815389555245ed740f1875c728879e4d9ef
victorvalenzuelamartinez <vvalen01@ucm.es>
2024-02-26 11:26:58 +0100

    Archivos iniciales añadidos al índice del repositorio

---
+++ practica git/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace practica_git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
